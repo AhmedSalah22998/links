6bd1420dadfeee8bf062b3547c6ce788e66c30e9
AhmedSalah22998 <159345207+AhmedSalah22998@users.noreply.github.com>
2024-02-25 13:16:24 +0300

    apppp

--- main.js
+++ main.js
@@ -27,11 +27,13 @@ function detectOS() {
   return "unknown";
 }
 
+const app = detectOS();
+
 // Example usage
 if (isMobileDevice()) {
   console.log("This is a mobile device.");
   document.body.innerHTML = "This is a mobile device ...";
-  document.body.innerHTML = "Operating System:" + detectOS();
+  document.body.innerHTML = "Operating System:" + app;
   console.log("Operating System:", detectOS());
 } else {
   document.body.innerHTML = "This is not a mobile device.";
